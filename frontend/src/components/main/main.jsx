9a50510efd0aa7afedb45d1dbc690f16972a600b
Alexisss_cn <nc4186624@gmail.com>
2023-11-01 09:53:17 +0100

    apartado quienes somos

--- frontend/src/components/main/main.jsx
+++ frontend/src/components/main/main.jsx
@@ -9,6 +9,18 @@ const Main = () => {
    <div>
       <img class="img" src={ citlam } alt=''/>
    </div>
+   <div className="quienes-somos-container">
+      <h1 className="title">Quiénes Somos</h1>
+      <div className="info-container">
+        <div className="left-info">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in arcu vel augue bibendum posuere.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis turpis eu metus tincidunt dapibus.</p>
+        </div>
+        <div className="right-info">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec massa sed libero imperdiet iaculis.</p>
+        </div>
+      </div>
+    </div>
    <div class="contenedor">
     <div class="equipo">
       <img class="logo" src={ robotica } alt=''/>
