3ab7756d751f8026c6c623c26336659c23303694
esteban ojeda <emiiiojeda615@gmail.com>
2023-11-01 11:39:07 -0300

    modificacion de nico

--- frontend/src/components/main/main.jsx
+++ frontend/src/components/main/main.jsx
@@ -9,54 +9,73 @@ const Main = () => {
    <div>
       <img class="img" src={ citlam } alt=''/>
    </div>
-   <div className="quienes-somos-container">
-      <h1 className="title">Quiénes Somos</h1>
-      <div className="info-container">
-        <div className="left-info">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in arcu vel augue bibendum posuere.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis turpis eu metus tincidunt dapibus.</p>
+   <div class="card-container">
+    <div class="card">
+        <div class="face front">
+            <img class="img" src={ robotica } alt=""/>
+            <h3>MMO</h3>
         </div>
-        <div className="right-info">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec massa sed libero imperdiet iaculis.</p>
+        <div class="face back">
+            <h3>MMO</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius harum molestiae iste, nihil doloribus fugiat distinctio ducimus maxime totam nulla fuga odio non aperiam eos?</p>
+            <div class="link">
+                <a href="#">Details</a>
+            </div>
         </div>
-      </div>
     </div>
-   <div class="contenedor">
-    <div class="equipo">
-      <img class="logo" src={ robotica } alt=''/>
-      <h2>MMO</h2>
-      <p>lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus</p>
-      <button class="mi-boton">Mas info</button>
+    <div class="card">
+        <div class="face front">
+        <img class="img" src={ robotica } alt=""/>
+            <h3>ROBOTICA</h3>
+        </div>
+        <div class="face back">
+            <h3>ROBOTICA</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius harum molestiae iste, nihil doloribus fugiat distinctio ducimus maxime totam nulla fuga odio non aperiam eos?</p>
+            <div class="link">
+                <a href="#">Details</a>
+            </div>
+        </div>
     </div>
-    
-    <div class="equipo">
-    <img class="logo" src={ robotica } alt=''/>
-      <h2>ROBOTICA</h2>
-      <p>lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus</p>
-      <button class="mi-boton">Mas info</button>
+    <div class="card">
+        <div class="face front">
+        <img class="img" src={ robotica } alt=""/>
+            <h3>CIENCIA</h3>
+        </div>
+        <div class="face back">
+            <h3>CIENCIA</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius harum molestiae iste, nihil doloribus fugiat distinctio ducimus maxime totam nulla fuga odio non aperiam eos?</p>
+            <div class="link">
+                <a href="#">Details</a>
+            </div>
+        </div>
     </div>
-    
-    <div class="equipo">
-    <img class="logo" src={ robotica } alt=''/>
-      <h2>CIENCIA</h2>
-      <p>lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus</p>
-      <button class="mi-boton">Mas info</button>
+    <div class="card">
+        <div class="face front">
+        <img class="img" src={ robotica } alt=""/>
+            <h3>INFORMATICA</h3>
+        </div>
+        <div class="face back">
+            <h3>INFORMATICA</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius harum molestiae iste, nihil doloribus fugiat distinctio ducimus maxime totam nulla fuga odio non aperiam eos?</p>
+            <div class="link">
+                <a href="#">Details</a>
+            </div>
+        </div>
     </div>
-    
-    <div class="equipo">
-    <img class="logo" src={ robotica } alt=''/>
-      <h2>INFORMATICA</h2>
-      <p>lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus</p>
-      <button class="mi-boton">Mas info</button>
+    <div class="card">
+        <div class="face front">
+        <img class="img" src={ robotica } alt=""/>
+            <h3>IMPRESION 3D</h3>
+        </div>
+        <div class="face back">
+            <h3>IMPRESION 3D</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius harum molestiae iste, nihil doloribus fugiat distinctio ducimus maxime totam nulla fuga odio non aperiam eos?</p>
+            <div class="link">
+                <a href="#">Details</a>
+            </div>
+        </div>
     </div>
-    
-    <div class="equipo">
-    <img class="logo" src={ robotica } alt=''/>
-      <h2>IMPRESION 3D</h2>
-      <p>lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus</p>
-      <button class="mi-boton">Mas info</button>
     </div>
-  </div>
  </>
   );
     };
