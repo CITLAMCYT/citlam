0841329d97a045de4fcf3c1d5c33c64d73ccae6f
esteban ojeda <emiiiojeda615@gmail.com>
2023-11-01 10:26:56 -0300

    main agregado a modificar

--- frontend/src/components/main/main.jsx
+++ frontend/src/components/main/main.jsx
@@ -1,13 +1,52 @@
 import React from 'react';
 import './Main.css';
-import citlam from '../../assets/citlam.jpg';
+import citlam from '../../assets/citlam.png';
+import robotica from '../../assets/robotica.jpg';
 
 const Main = () => {
     return (
+ <>
    <div>
       <img class="img" src={ citlam } alt=''/>
+   </div>
+   <div class="contenedor">
+    <div class="equipo">
+      <img class="logo" src={ robotica } alt=''/>
+      <h2>MMO</h2>
+      <p>lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus</p>
+      <button class="mi-boton">Mas info</button>
     </div>
+    
+    <div class="equipo">
+    <img class="logo" src={ robotica } alt=''/>
+      <h2>ROBOTICA</h2>
+      <p>lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus</p>
+      <button class="mi-boton">Mas info</button>
+    </div>
+    
+    <div class="equipo">
+    <img class="logo" src={ robotica } alt=''/>
+      <h2>CIENCIA</h2>
+      <p>lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus</p>
+      <button class="mi-boton">Mas info</button>
+    </div>
+    
+    <div class="equipo">
+    <img class="logo" src={ robotica } alt=''/>
+      <h2>INFORMATICA</h2>
+      <p>lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus</p>
+      <button class="mi-boton">Mas info</button>
+    </div>
+    
+    <div class="equipo">
+    <img class="logo" src={ robotica } alt=''/>
+      <h2>IMPRESION 3D</h2>
+      <p>lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus lorem insus</p>
+      <button class="mi-boton">Mas info</button>
+    </div>
+  </div>
+ </>
   );
-};
+    };
 
 export default Main;
