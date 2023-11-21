69be04a2aae5d72f86121fe5738a21caf1d8708d
Alexisss_cn <nc4186624@gmail.com>
2023-11-21 08:12:44 +0100

    quienes somos

--- frontend/src/components/main/main.jsx
+++ frontend/src/components/main/main.jsx
@@ -1,40 +1,25 @@
 import React from 'react';
-import './Inicio.css';
-import { ciencia, MMO, programacion, impresora, robotica, citlam } from '../../assets/index.js';
-import { Link } from 'react-router-dom';
+import './Main.css';
+import citlam from '../../assets/citlam.png';
+import robotica from '../../assets/robotica.jpg';
 
-const Inicio = () => {
+const Main = () => {
     return (
  <>
    <div>
       <img class="img" src={ citlam } alt=''/>
-   </div>
-   <div id="quienes-somos" className="quienes-somos-container">
-      <h1 className="title">Quiénes Somos</h1>
-      <div className="info-container">
-        <div className="left-info">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in arcu vel augue bibendum posuere.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis turpis eu metus tincidunt dapibus.</p>
-        </div>
-        <div className="right-info">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec massa sed libero imperdiet iaculis.</p>
-        </div>
-      </div>
-    </div>
-   <div id="nuestro-equipo" class="title-container">
-    <h1 className='title'>Nuestro Equipo</h1>
-   </div>
-   <div class="card-container"> 
+   </div> 
+   <div class="card-container">
     <div class="card">
         <div class="face front">
-            <img class="img" src={ MMO } alt=""/>
+            <img class="img" src={ robotica } alt=""/>
             <h3>MMO</h3>
         </div>
         <div class="face back">
             <h3>MMO</h3>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius harum molestiae iste, nihil doloribus fugiat distinctio ducimus maxime totam nulla fuga odio non aperiam eos?</p>
             <div class="link">
-            <Link to="/Mmo">mas info</Link>
+                <a href="#">Details</a>
             </div>
         </div>
     </div>
@@ -47,53 +32,52 @@ const Inicio = () => {
             <h3>ROBOTICA</h3>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius harum molestiae iste, nihil doloribus fugiat distinctio ducimus maxime totam nulla fuga odio non aperiam eos?</p>
             <div class="link">
-            <Link to="/Robotica">mas info</Link>
+                <a href="#">Details</a>
             </div>
         </div>
     </div>
     <div class="card">
         <div class="face front">
-        <img class="img" src={ ciencia } alt=""/>
+        <img class="img" src={ robotica } alt=""/>
             <h3>CIENCIA</h3>
         </div>
         <div class="face back">
             <h3>CIENCIA</h3>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius harum molestiae iste, nihil doloribus fugiat distinctio ducimus maxime totam nulla fuga odio non aperiam eos?</p>
             <div class="link">
-            <Link to="/Ciencia">mas info</Link>
+                <a href="#">Details</a>
             </div>
         </div>
     </div>
     <div class="card">
         <div class="face front">
-        <img class="img" src={ programacion } alt=""/>
+        <img class="img" src={ robotica } alt=""/>
             <h3>INFORMATICA</h3>
         </div>
         <div class="face back">
             <h3>INFORMATICA</h3>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius harum molestiae iste, nihil doloribus fugiat distinctio ducimus maxime totam nulla fuga odio non aperiam eos?</p>
             <div class="link">
-            <Link to="/Informatica">mas info</Link>
+                <a href="#">Details</a>
             </div>
         </div>
     </div>
     <div class="card">
         <div class="face front">
-        <img class="img" src={ impresora } alt=""/>
+        <img class="img" src={ robotica } alt=""/>
             <h3>IMPRESION 3D</h3>
         </div>
         <div class="face back">
             <h3>IMPRESION 3D</h3>
-            <p>Lorem ipsum dolor sit, Eius harum molestiae iste, nihil doloribus fugiat distinctio ducimus maxime totam nulla fuga odio non aperiam eos?</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius harum molestiae iste, nihil doloribus fugiat distinctio ducimus maxime totam nulla fuga odio non aperiam eos?</p>
             <div class="link">
-            <Link to="/Impresion">mas info</Link>
+                <a href="#">Details</a>
             </div>
         </div>
     </div>
     </div>
-
  </>
   );
     };
 
-export default Inicio;
+export default Main;
--- frontend/src/views/quienes/Quienes.jsx
+++ frontend/src/views/quienes/Quienes.jsx
@@ -1,19 +1,55 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './Quienes.css';
 
 const Quienes = () => {
-    return (
-      <> 
-      
-      </>
+  const questions = [
+    {
+      id: 1,
+      question: '¿quien es el mas lindo del citlam?',
+      answer:
+        'el mas lindo del citlam es Ezequielito',
+    },
+    {
+      id: 2,
+      question: '¿el mas gay del citlam?',
+      answer:
+        'Emi',
+    },
+    {
+      id: 3,
+      question: '¿quien es el mas gay del mundo ?',
+      answer:
+        'Emi te dije',
+    },
+  ];
+
+  const [activeQuestion, setActiveQuestion] = useState(null);
+
+  const handleToggleClick = (questionId) => {
+    setActiveQuestion(questionId === activeQuestion ? null : questionId);
+  };
+
+  return (
+    <div className="container2">
+      <div className="container1">
+        {questions.map((q) => (
+          <div className="wrapper1" key={q.id}>
+            <button
+              className={`toggle ${q.id === activeQuestion ? 'active' : ''}`}
+              onClick={() => handleToggleClick(q.id)}
+            >
+              {q.question}
+            </button>
+            {q.id === activeQuestion && (
+              <div className="content1">
+                <p>{q.answer}</p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
 export default Quienes;
-
-
-
-
-
-
-
